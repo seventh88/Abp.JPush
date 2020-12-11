308c0fbb0eeeb580c2c879eef7970825e19a1f23
seventh88 <5856868x@gmail.com>
2020-12-11 10:42:12 +0800

    Init Project

---
+++ src/Light.Abp.Captcha/AbpJPushModule.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace Light.Abp.JPush
+{
+    public class AbpJPushModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+
+        }
+    }
+}
